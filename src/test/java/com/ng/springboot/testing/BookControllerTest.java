fccc26534dcbd583acd208d1ec0d71c8c9d646de
nirmalgurjar <nirmalgurjar@localhost>
2024-09-24 01:29:02 +0530

    aligment corrected

--- src/test/java/com/ng/springboot/testing/BookControllerTest.java
+++ src/test/java/com/ng/springboot/testing/BookControllerTest.java
@@ -47,40 +47,39 @@ class BookControllerTest {
 	public void init() throws Exception {
 	}
 
-	@Test //@formatter:on
+	@Test //@formatter:off
 	void testGetBook() throws Exception {
 
 		Book book = Book.builder().bookId(1).bookName("book1").build();
 
 		when(bookService.getBookById(1)).thenReturn(Optional.of(book));
 
-		ResultActions response = mockMvc.perform(get("/book/1")).andDo(MockMvcResultHandlers.print()); // this will
-																										// print object
-
+		ResultActions response = mockMvc.perform(get("/book/1")).andDo(MockMvcResultHandlers.print()); // this will print req res
+																										
 		response.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookId", CoreMatchers.is(book.getBookId())))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookName", CoreMatchers.is(book.getBookName())));
 
 	}
 
-	@Test //@formatter:on
+	@Test //@formatter:Off
 	void testGetAllBooks() throws Exception {
 
 		List<Book> bookList = List.of(new Book(1, "book1"), new Book(2, "book2"));
 
 		when(bookService.getAllBooks()).thenReturn(bookList);
 
-		ResultActions response = mockMvc.perform(get("/book")).andDo(MockMvcResultHandlers.print()); // this will print
-																										// object
+		ResultActions response = mockMvc.perform(
+												get("/book")
+												).andDo(MockMvcResultHandlers.print()); // this will print
+																										
 
 		response.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(bookList.size())))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].bookId", CoreMatchers.is(bookList.get(0).getBookId())))
-				.andExpect(
-						MockMvcResultMatchers.jsonPath("$[0].bookName", CoreMatchers.is(bookList.get(0).getBookName())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].bookName", CoreMatchers.is(bookList.get(0).getBookName())))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].bookId", CoreMatchers.is(bookList.get(1).getBookId())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[1].bookName",
-						CoreMatchers.is(bookList.get(1).getBookName())));
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].bookName", CoreMatchers.is(bookList.get(1).getBookName())));
 
 	}
 
@@ -92,9 +91,11 @@ class BookControllerTest {
 		when(bookService.createBook(Mockito.any(Book.class))).thenReturn(book);
 
 		ResultActions response = mockMvc.perform(
-				post("/book").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(book)))
-				.andDo(MockMvcResultHandlers.print()); // this will print object
-
+	                                             post("/book")
+	                                             .contentType(MediaType.APPLICATION_JSON)
+	                                             .content(objectMapper.writeValueAsString(book))
+	                             				).andDo(MockMvcResultHandlers.print()); // this will print object
+		
 		response.andExpect(MockMvcResultMatchers.status().isCreated())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookId", CoreMatchers.is(book.getBookId())))
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookName", CoreMatchers.is(book.getBookName())));
@@ -111,8 +112,10 @@ class BookControllerTest {
 		when(bookService.updateBook(Mockito.any(Book.class))).thenReturn(book);
 
 		ResultActions response = mockMvc.perform(
-				put("/book").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(book)))
-				.andDo(MockMvcResultHandlers.print()); // this will print object
+												put("/book")
+												.contentType(MediaType.APPLICATION_JSON)
+												.content(objectMapper.writeValueAsString(book))
+												).andDo(MockMvcResultHandlers.print()); // this will print object
 
 		response.andExpect(MockMvcResultMatchers.status().isAccepted())
 				.andExpect(MockMvcResultMatchers.jsonPath("$.bookId", CoreMatchers.is(book.getBookId())))
@@ -128,8 +131,9 @@ class BookControllerTest {
 
 		doNothing().when(bookService).deleteBook(1);
 
-		ResultActions response = mockMvc.perform(delete("/book/1")).andDo(MockMvcResultHandlers.print()); // this will
-		// print object
+		ResultActions response = mockMvc.perform(
+												delete("/book/1")
+												).andDo(MockMvcResultHandlers.print()); // this will print object
 
 		response.andExpect(MockMvcResultMatchers.status().isAccepted());
 
